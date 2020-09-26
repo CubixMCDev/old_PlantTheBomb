af596eef8fc3ccab0175ff69a96468322c8b4533
Karamouche <62983822+Karamouche@users.noreply.github.com>
2020-09-26 20:19:52 +0200

    Smoke blinking fix

--- src/fr/karamouche/plantthebomb/objects/grenade/Smoke.java
+++ src/fr/karamouche/plantthebomb/objects/grenade/Smoke.java
@@ -5,9 +5,9 @@ import javafx.scene.shape.Sphere;
 import net.minecraft.server.v1_8_R3.EnumParticle;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.Vector;
 
 /*
 (x - a)² + (y - b)² + (z - c)² = r²  where (a, b, c) represents the center of the sphere .
@@ -25,24 +25,24 @@ public class Smoke extends Grenade{
             int time = 0;
             @Override
             public void run() {
-                if(time == 15*10) {
+                if(time == 12*5) {
                     smoke.removeEffect();
                 }
                 loc.add(0, 1, 0);
-                for (double i = 0; i <= Math.PI; i += Math.PI / 30) {
+                for (double i = 0; i <= Math.PI; i += Math.PI / 25) {
                     double radius = 2.5*Math.sin(i);
                     double y = 2.5*Math.cos(i);
-                    for (double a = 0; a < Math.PI * 2; a+= Math.PI / 30) {
+                    for (double a = 0; a < Math.PI * 2; a+= Math.PI / 25) {
                         double x = Math.cos(a) * radius;
                         double z = Math.sin(a) * radius;
                         loc.add(x, y, z);
-                        ParticleEffect packet1 = new ParticleEffect(EnumParticle.CLOUD, loc,
-                                0, 0, 0, 0, 1);
-                        for(Player p : Bukkit.getOnlinePlayers())
-                            packet1.sendToPlayer(p);
+                        if(loc.getBlock().getType().equals(Material.AIR) || loc.getBlock() == null) {
+                            ParticleEffect packet1 = new ParticleEffect(EnumParticle.CLOUD, loc,
+                                    0, 0, 0, 0, 1);
+                            for (Player p : Bukkit.getOnlinePlayers())
+                                packet1.sendToPlayer(p);
+                        }
                         loc.subtract(x, y, z);
-
-                        // display particle at 'location'.
                     }
                 }
 
@@ -56,7 +56,8 @@ public class Smoke extends Grenade{
     public void setEffect() {
         super.setEffect();
         this.setLunch(true);
-        this.run.runTaskTimer(myPlugin, 0, 2);
+        this.run.runTaskTimer(myPlugin, 0, 4);
+        System.out.println(this.run.getTaskId());
 
 
     }
